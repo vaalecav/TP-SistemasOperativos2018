93edaeb8d8f1a082bff37e27887dd85a7fab81ad
Xhisth <alexisgomez@est.frba.utn.edu.ar>
2018-04-11 17:15:49 -0300

    Agrego consola al planificador

--- planificador/src/planificador.c
+++ planificador/src/planificador.c
@@ -11,7 +11,22 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int main(void) {
-	puts("!!!Hello World!!!"); /* prints !!!Hello World!!! */
-	return EXIT_SUCCESS;
+#include <readline/readline.h>
+#include <readline/history.h>
+
+int main() {
+	char* line;
+	puts("Inicializando Planificador...");
+	while (1) {
+		line = readline("user@planificador: ");
+		if (strcmp(line, "exit") == 0) {
+			free(line);
+			puts("Finalizando Planificador...");
+			exit(0);
+		}
+		printf("El comando %s no es un comando valido.\n", line);
+		free(line);
+	}
+
+	return 0;
 }
